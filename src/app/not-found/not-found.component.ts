9bcb13afdf9dd28040e65b0148767efddb96ba16
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-09-15 19:34:09 +0100

    Not found page back button

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent { }
+export class NotFoundComponent {
+
+  constructor(private location: Location) {}
+  back() {
+    this.location.back();
+  }
+}
