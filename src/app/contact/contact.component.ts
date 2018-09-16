f716de709aafb047163a18a095be0ac217b0ee80
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-09-16 05:25:54 +0100

    cleanup

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { Contact } from './contact';
-import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
-  email = new Contact('', '', '');
-  emailControl = new FormControl(Validators.required, Validators.email);
-  showForm = false;
-
-  onSubmit() {
-    console.log(this.email);
-  }
-
-  getErrorMessage(control: any) {
-    return control.hasError('required') ? 'Cannot be blank!' :
-    control.hasError('email') ? 'Please enter a valid email!' : '';
-  }
-}
+export class ContactComponent { }
