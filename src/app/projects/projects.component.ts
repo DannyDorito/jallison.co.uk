b98fa04632558ff5ca0875400332d804d17c55be
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2018-09-22 16:16:31 +0100

    get GitHub repo

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from './projects.service';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent { }
+export class ProjectsComponent implements OnInit {
+  description: string;
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit() {
+    this.getRepo('DannyDorito', 'ARMA-3-Startup-and-Restart-Script');
+  }
+
+  getRepo(username: string, repo: string) {
+    this.projectsService.getRepo(username, repo).subscribe(result => {
+      this.description = result.description;
+    });
+  }
+}
