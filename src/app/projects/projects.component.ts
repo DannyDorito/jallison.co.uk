cdeec5c18e314f9b2b15355d279ea8ccf065d941
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-06-08 14:58:32 +0100

    removed unused github profile code

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ProjectsService } from './projects.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent implements OnInit {
-  allRepos: GitHubRepo[];
-  constructor(private projectsService: ProjectsService) {}
-
-  ngOnInit() {
-
-  }
-
-  getRepo(username: string, repo: string) {
-    this.projectsService.getRepo(username, repo).subscribe(result => {
-      // this.description = result.description;
-    }, error => {
-      console.log(error);
-    });
-  }
-
-  getAllRepos(username: string) {
-    this.projectsService.getAllPublicRepos(username).subscribe(result => {
-      this.allRepos = result;
-    }, error => {
-      console.log(error);
-    });
-  }
-}
+export class ProjectsComponent {}
