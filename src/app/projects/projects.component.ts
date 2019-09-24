964312705d4fcc282bc834a9cce876f4632e9bf3
DannyDorito <32311622+DannyDorito@users.noreply.github.com>
2019-09-24 12:50:33 +0100

    Update projects.component.ts

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,18 +7,5 @@ import { ProjectsService } from './projects.service';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-  repos: GithubRepo;
   constructor(private projectsService: ProjectsService) {}
-
-  async ngOnInit() {
-    // this.repos = await this.getGitHubRepos('DannyDorito');
-    // console.log(this.repos);
-  }
-
-  async getGitHubRepos(username: string): Promise<GithubRepo> {
-    this.projectsService.getGitHubRepos(username).subscribe(result => {
-      return result;
-    });
-    return undefined;
-  }
 }
